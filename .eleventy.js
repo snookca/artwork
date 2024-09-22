a1c0de3aaa3eb5ca80fb6a5a608ec222918017bd
Jonathan Snook <jonathan@snook.ca>
2024-09-22 19:45:47 -0400

    More basics

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,13 @@
-/*
 const { DateTime } = require("luxon");
+/*
 const Image = require("@11ty/eleventy-img");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 */
 module.exports = function(eleventyConfig) {
-  /*
-  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addFilter('prettyDate', (dateObj) => {
     var d = DateTime.fromJSDate(dateObj, {zone:'utc'});
     return d.toFormat("LLLL d, kkkk");
   });
-*/
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("favicon.ico");
---
+++ piece/scsc.md
@@ -0,0 +1,15 @@
+---
+layout: artwork.njk
+tags: artwork
+title: SCSC in Gold and Black
+artist: Boyd Waites
+artist_url: 
+img: 2023/coiffure.jpg
+imgthumb: 2023/coiffure_thumb.jpg
+imgurl:
+
+date: 2023-09-14
+---
+
+Walking around Paris, looking for delightful coffee shops or a park to sit in, and happen upon a nook or two where a doorway leads to a hidden alcove. 
+A hairdresser or a restaurant or some other establishment lies deep within the heart of the city.
